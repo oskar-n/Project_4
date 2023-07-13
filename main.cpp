b267e915910bd8f2c51ddfb7806d0166a727f312
Hubert Rotkiewicz <hrotkiewicz@gmail.com>
2023-07-13 17:40:44 +0200

    small refactor

--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
 
 #include "models.hpp"
 
+template <typename... Args>
+void draw(sf::RenderWindow &win, Args... args){
+    (args.draw(win), ...);
+}
+
 int main()
 {
-int elevator_pos_x=SCREEN_WIDTH/2-ELEVATOR_WIDTH;
-int elevator_pos_y=SCREEN_HEIGHT-ELEVATOR_HIGHT*2-5;
-sf::RenderWindow window(sf::VideoMode(1500, 800), "Elevator");
-elevator m_elevator(elevator_pos_x,elevator_pos_y, sf::Color::Blue);
-button b1(50, 50, sf::Color::Red, '1');
-button b2(50, 150, sf::Color::Red, '2');
-while (window.isOpen())
-{
-sf::Event event;
-while (window.pollEvent(event))
-{
-if (event.type == sf::Event::Closed)
-    window.close();
-if (event.type==sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+    int elevator_pos_x=SCREEN_WIDTH/2-ELEVATOR_WIDTH;
+    int elevator_pos_y=SCREEN_HEIGHT-ELEVATOR_HIGHT*2-5;
+    sf::RenderWindow window(sf::VideoMode(1500, 800), "Elevator");
+    Elevator elevator(elevator_pos_x,elevator_pos_y, sf::Color::Blue);
+    Button b1(50, 50, sf::Color::Red, '1');
+    Button b2(50, 150, sf::Color::Red, '2');
+    while (window.isOpen())
     {
-        b1.is_pressed(window, m_elevator, 100);
-        b2.is_pressed(window, m_elevator, 300);
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type==sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+            {
+                b1.is_pressed(window, elevator, 100);
+                b2.is_pressed(window, elevator, 300);
+            }
+
+        }
+        elevator.moving();
+
+        window.clear();
+        draw(window,elevator, b1, b2);
+        window.display();
     }
-        
-}
-m_elevator.moving();
-
-window.clear();
-
-b1.draw_button(window);
-b2.draw_button(window);
-m_elevator.draw_elevator(window);
-window.display();
-}
-return 0;
+    return 0;
 }
--- models.hpp
+++ models.hpp
@@ -1,40 +1,40 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#define SCREEN_WIDTH 1500
-#define SCREEN_HEIGHT 800
-#define BUTTON_WIDTH 50
-#define ELEVATOR_WIDTH 200
-#define ELEVATOR_HIGHT 100
 
+constexpr int SCREEN_WIDTH   =1500;
+constexpr int SCREEN_HEIGHT  = 800;
+constexpr int BUTTON_WIDTH   =50;
+constexpr int ELEVATOR_WIDTH =200;
+constexpr int ELEVATOR_HIGHT =100;
 
-class elevator
+class Elevator
 {
     private:
-    int x_=0;
-    int y_=0;
+    int m_x=0;
+    int m_y=0;
     int m_width=ELEVATOR_WIDTH;
     int m_hight=ELEVATOR_HIGHT;
     sf::Color m_color;
     sf::RectangleShape m_rectangle;
     public:
-    elevator(int x, int y, sf::Color color)
+    Elevator(int x, int y, sf::Color color)
     {
-        x_=x;
-        y_=y;
+        m_x=x;
+        m_y=y;
         m_color=color;
         m_rectangle.setSize(sf::Vector2f(m_width, m_hight));
-        m_rectangle.setPosition(x_, y_);
+        m_rectangle.setPosition(m_x, m_y);
     }
     
     void moving ()
     {   
     sf::sleep(sf::milliseconds(2)); 
-    if (m_rectangle.getPosition().y>y_)
+    if (m_rectangle.getPosition().y>m_y)
     {
         m_rectangle.move(0,-1);
     }
-    if (m_rectangle.getPosition().y<y_)
+    if (m_rectangle.getPosition().y<m_y)
     {
         m_rectangle.move(0,1);
     }
@@ -42,11 +42,11 @@ class elevator
     }
     void set_pos(int x, int y)
     {   
-        x_=x;
-        y_=y;
+        m_x=x;
+        m_y=y;
     }
 
-    void draw_elevator(sf::RenderWindow &window)
+    void draw(sf::RenderWindow &window)
      {
         m_rectangle.setSize(sf::Vector2f(m_width, m_width));
         m_rectangle.setFillColor(m_color);
@@ -57,7 +57,7 @@ class elevator
 
 };
 
-class button{
+class Button{
     private: 
     int m_pos_x;
     int m_pos_y;
@@ -67,7 +67,8 @@ class button{
     sf::Text m_text;
     sf::Font m_font;
     public:
-    button(int pos_x, int pos_y, sf::Color color,  char num)
+
+    Button(int pos_x, int pos_y, sf::Color color,  char num)
     {
         m_pos_x=pos_x;
         m_pos_y=pos_y;
@@ -81,10 +82,9 @@ class button{
         m_text.setCharacterSize(50);
         m_text.setFillColor(sf::Color::White);
         m_text.setString(num);
-
     }
 
-    void draw_button(sf::RenderWindow &window)
+    void draw(sf::RenderWindow &window)
     {
         m_rectangle.setFillColor(m_color);
         m_rectangle.setPosition(m_pos_x, m_pos_y);
@@ -93,7 +93,7 @@ class button{
         window.draw(m_text);
     }
     
-    void is_pressed(sf::RenderWindow &window, elevator &m_elevator, int post_y)
+    void is_pressed(sf::RenderWindow &window, Elevator &m_elevator, int post_y)
     {
         sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
         if (mouse_pos.x>m_pos_x && mouse_pos.x<m_pos_x+m_width && mouse_pos.y>m_pos_y && mouse_pos.y<m_pos_y+m_width)
