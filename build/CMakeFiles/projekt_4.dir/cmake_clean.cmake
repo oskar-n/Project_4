9ecf6a5aca260788c02bbaa454b643e1637b88d9
Hubert Rotkiewicz <hrotkiewicz@gmail.com>
2023-08-10 18:44:41 +0200

    added debug option

---
+++ build/CMakeFiles/projekt_4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt_4.dir/main.cpp.o"
+  "CMakeFiles/projekt_4.dir/main.cpp.o.d"
+  "projekt_4"
+  "projekt_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
